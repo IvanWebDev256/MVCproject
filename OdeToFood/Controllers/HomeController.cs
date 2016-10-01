919a48817a6159fabb8d3a27756f41b413763c4e
IvanWebDev256 <john.parker256@mail.ru>
2016-10-01 12:56:21 +0300

    Add Async Search

--- OdeToFood/Controllers/HomeController.cs
+++ OdeToFood/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace OdeToFood.Controllers
                    }
                             );
 
+            if (Request.IsAjaxRequest())
+            {
+                return PartialView("_Restaurants", model);
+            }
+
             return View(model);
         }
 
@@ -57,72 +62,3 @@ namespace OdeToFood.Controllers
         }
     }
 }
-
-
-//using OdeToFood.Models;
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Web;
-//using System.Web.Mvc;
-
-//namespace OdeToFood.Controllers
-//{
-//    public class HomeController : Controller
-//    {
-//        OdeToFoodDb _db = new OdeToFoodDb(); 
-
-//        public ActionResult Index(string searchTerm = null)
-//        {
-//            //var model = from r in _db.Restaurants // retrieved all restaurants from SQL table
-//            //            orderby r.Reviews.Average(review => review.Rating) descending
-//            //            select new RestaurantListViewModel
-//            //            {
-//            //                Id = r.Id,
-//            //                Name = r.Name,
-//            //                City = r.City,
-//            //                Country = r.Country,
-//            //                CountOfReviews = r.Reviews.Count
-//            //            };
-//            var model =
-//                _db.Restaurants
-//                .OrderByDescending(r => r.Reviews.Average(review => review.Rating))
-//                .Where(r => searchTerm == null || r.Name.StartsWith(searchTerm))
-//                .Take(10)
-//                .Select(r => new RestaurantListViewModel
-//                {
-//                    Id = r.Id,
-//                    Name = r.Name,
-//                    City = r.City,
-//                    Country = r.Country,
-//                    CountOfReviews = r.Reviews.Count
-//                });
-//            return View(model);
-//        }
-
-//        protected override void Dispose(bool disposing)
-//        {
-//            if(_db != null)
-//            {
-//                _db.Dispose(); // clean unused resources
-//            }
-//            base.Dispose(disposing);
-//        }
-
-//        public ActionResult About()
-//        {
-//            var model = new AboutModel();
-//            model.Name = "Ivan";
-//            model.Location = "Kiev, UA";
-
-//            return View(model);
-//        }
-
-//        public ActionResult Contact()
-//        {
-//            ViewBag.Message = "Your contact page.";
-
-//            return View();
-//        }
-//    }
-//}
---
+++ OdeToFood/Scripts/otf.js
@@ -0,0 +1,23 @@
+﻿
+$(function () {
+
+    var ajaxFormSubmit = function () {
+        var $form = $(this); // reference to form which is submitted\
+        var options = {
+            url: $form.attr("action"),
+            type: $form.attr("method"),
+            data: $form.serialize()
+        };
+
+        $.ajax(options).done(function (data) {
+            var $target = $($form.attr("data-otf-target")); // DOM elements need to be updated
+            var $newHtml = $(data);
+            $target.replaceWith($newHtml);
+            $newHtml.effect("highlight");
+        });
+
+        return false;
+    };
+
+    $("form[data-otf-ajax='true']").submit(ajaxFormSubmit);
+});
